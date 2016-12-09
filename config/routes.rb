05640b7c461787916b02d55f85c9e4fff76fc15f
Marco Rosner <marco.rosner@gmail.com>
2016-12-09 13:51:45 -0300

    Adding production secret

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :accounts
   resources :bills
   resources :users
-  root 'user#index'
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
